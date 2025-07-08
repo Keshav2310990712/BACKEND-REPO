3f52f80c37a9a4ecded1394a2fb680194e15ced1
keshav <keshav0712.be23@chitkara.edu.in>
2025-07-08 09:31:06 +0530

    added

--- LEC_1/eventloop.js
+++ LEC_1/eventloop.js
@@ -6,6 +6,11 @@ setTimeout(()=>{
 setImmediate(()=>{
     console.log("set immediate callback")    //calling phase k baad run hota hai
 })
+function dosometask(){
+    return new Promise((resolve,reject)=>{
+        resolve("promise chla")
+    })
+}
 fs.readFile("demo.txt",(data)=>{
     console.log("poll phase callback");
     setTimeout(()=>{
@@ -16,5 +21,14 @@ fs.readFile("demo.txt",(data)=>{
     })
 })
 console.log("end")
+dosometask().then((res)=>{
+    console.log(res)
+})
+.catch((err)=>{
+    console.log(err)
+})
+process.nextTick(()=>{
+    console.log("next tick")
+})
 
 
